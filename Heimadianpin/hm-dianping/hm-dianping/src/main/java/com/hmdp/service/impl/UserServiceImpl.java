6f92eda1a323965dd22c1a6b5ebc0155babec011
Charmmy-ing <lc5207242@gmail.com>
2026-09-06 16:53:07 +0800

    9/6v1_借助session实现平台的登录功能（验证手机号，生成验证码，手机号验证码校对），并借助springboot实现登录状态校对

--- Heimadianpin/hm-dianping/hm-dianping/src/main/java/com/hmdp/service/impl/UserServiceImpl.java
+++ Heimadianpin/hm-dianping/hm-dianping/src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -1,9 +1,11 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.util.RandomUtil;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.hmdp.dto.LoginFormDTO;
 import com.hmdp.dto.Result;
+import com.hmdp.dto.UserDTO;
 import com.hmdp.entity.User;
 import com.hmdp.mapper.UserMapper;
 import com.hmdp.service.IUserService;
@@ -12,6 +14,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpSession;
+
+import static com.hmdp.utils.SystemConstants.USER_NICK_NAME_PREFIX;
 
 /**
  * <p>
@@ -34,7 +38,7 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
            }
         //3.如果符合规则，生成短信验证码并保存验证码到session
          String code = RandomUtil.randomNumbers(6);
-           session.setAttribute("code_"+phone,code);
+           session.setAttribute("code",code);
         //4.发送验证码到手机号
         log.info("发送验证码到手机号{}，验证码为{}",phone,code);
         //5.返回成功信息
@@ -50,21 +54,32 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
             return Result.fail("手机号格式错误");
         }
         //2.校验验证码是否正确
-        String cashCode = (String) session.getAttribute("code");
+        Object cashCode = session.getAttribute("code");
         String code = loginForm.getCode();
-        if(!code.equals(cashCode)){
+        if(cashCode == null || !code.equals(cashCode.toString())){
             //3.不一致，返回错误信息
             return Result.fail("验证码错误");
         }
-
-
         //4.一致，根据手机号查询用户是否存在
+        //在数据库中，查询用户是否存在 Select * from user where phone = (phone);
+        User user = query().eq("phone",phone).one();
+        //5.不存在，创建新用户并保存到数据库
+        if(user == null){
+            //6.创建新用户并拿到
+            user=createUserWithPhone(phone);
+        }
+        //6.保存用户到session
+        session.setAttribute("user", BeanUtil.copyProperties(user, UserDTO.class));
 
+        return Result.ok();
+    }
 
-        //5.如果不存在，创建新用户并保存到数据库
-
-        //6.如果存在，返回成功信息
-
-        return null;
+    private User createUserWithPhone(String phone) {
+        User user = new User();
+        user.setPhone(phone);
+        user.setNickName(USER_NICK_NAME_PREFIX+RandomUtil.randomString(10));
+        //7.保存用户到数据库
+        save(user);
+        return user;
     }
 }
